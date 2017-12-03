d1d212862772b22f7c98531970c2837da5b67c73
Aviio <connorwilson1@googlemail.com>
2017-12-03 13:16:25 +0000

    Initial Commit

---
+++ AdventOfCode1.Tests/Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace AdventOfCode1.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase("1122", "3")]
+        [TestCase("1111", "4")]
+        [TestCase("1234", "0")]
+        [TestCase("91212129", "9")]
+        public void TestCaptcha(string captcha, string result)
+        {
+            //arrange
+            var captchaSolver = new CaptchaSolver();
+            //act
+            var captchaResult = captchaSolver.SolveCatpcha(captcha);
+            //assert
+            Assert.AreEqual(result, captchaResult);
+        
+        }
+
+    }
+}
---
+++ AdventOfCode1/CaptchaSolver.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode1
+{
+    public class CaptchaSolver
+    {
+        public string SolveCatpcha(string captcha)
+        {
+            var captchaArray = captcha.ToCharArray();
+            int total = 0;
+            for (var i = 0; i <= (captchaArray.Length - 1); i++)
+            {
+                var mod = i == (captchaArray.Length - 1) ? 0 : i + 1;
+                if (captchaArray[mod] == captchaArray[i])
+                {
+                    total = total + int.Parse(captchaArray[i].ToString());
+                }
+            }
+            return total.ToString();
+        }
+    }
+}
